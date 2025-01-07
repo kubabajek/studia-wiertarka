da7835ab3ac25acc875a44328d3b16ec331517e4
kubabajek <bajek@student.agh.edu.pl>
2025-01-07 15:40:07 +0100

    add warning

--- wiertarka_bajka/Core/Inc/uart_data_handler.h
+++ wiertarka_bajka/Core/Inc/uart_data_handler.h
@@ -9,3 +9,6 @@ void uart_data_handler_init(); //configure uart and start transmission
 void uart_data_handler_register_callback_speed(UART_CB_SPEED function_speed_handler); //pass once any function returning void and getting uint8_t to call it with percent value after change
 void uart_data_handler_register_callback_power(UART_CB_POWER function_power_handler); //pass function taking bool, returning void with handling value true - power on, false - power off
 void uart_data_handler_register_callback_direction(UART_CB_DIRECTION function_direction_handler); //pass function taking bool, returning void with handling direction true - right, false - left
+
+
+//UWAGA! ZMIENIONO NAZWĘ FUNKCJI uart_data_handler_register_callback NA uart_data_handler_register_callback_speed
